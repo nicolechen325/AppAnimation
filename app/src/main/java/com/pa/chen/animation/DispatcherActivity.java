2e3c2ce92899f4d63f2a081879c42b0c72a91bf2
Nico <chenguang0508@163.com>
2018-12-18 22:15:43 +0800

    增加滑动进入和一个图片旋转动画加载框

--- app/src/main/java/com/pa/chen/animation/DispatcherActivity.java
+++ app/src/main/java/com/pa/chen/animation/DispatcherActivity.java
@@ -1,11 +1,13 @@
 package com.pa.chen.animation;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.pa.chen.animation.container.ContainerActivity;
 import com.pa.chen.animation.sample.ScrollerActivity;
@@ -13,6 +15,7 @@ import com.pa.chen.animation.explosion.ExplosionActivity;
 import com.pa.chen.animation.loading.LoadingActivity;
 import com.pa.chen.animation.sample.PropertyAnimActivity;
 import com.pa.chen.animation.sample.TweenAnimActivity;
+import com.pa.chen.animation.widget.ImageProgressDialog;
 
 //动画主页面
 public class DispatcherActivity extends Activity {
@@ -28,12 +31,26 @@ public class DispatcherActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        final ImageProgressDialog prDialog = new ImageProgressDialog(DispatcherActivity.this,
+                R.style.dialog);
+        prDialog.setCancelable(false);
+        prDialog.show();
+        prDialog.setContentText("正在加载,请稍后..");//在show之后，否则会空指针.
+        prDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                prDialog.stopRotate();
+            }
+        });
+
         mBtnProperty = (Button) findViewById(R.id.btn_anim_property);
         mBtnTween = (Button) findViewById(R.id.btn_anim_tween);
         mBtnScroller= (Button) findViewById(R.id.btn_scroller);
         mBtnExplosion = (Button) findViewById(R.id.btn_explosion);
         mBtnShowLoading = (Button) findViewById(R.id.btn_showloading);
         mBtnContainer = (Button) findViewById(R.id.btn_container);
+
     }
 
     public void btnPropertyAnim(View view) {
