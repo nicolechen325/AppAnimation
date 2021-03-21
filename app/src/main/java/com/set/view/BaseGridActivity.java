ca0b9816bd4c8ce777df8c9553860890ebb9badb
chenguang <chenguang0508@163.com>
2021-03-21 22:24:43 +0800

    分割线

--- app/src/main/java/com/set/view/BaseGridActivity.java
+++ app/src/main/java/com/set/view/BaseGridActivity.java
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.set.view.scroll.adapter.RecycleViewDivider;
 import com.set.view.scroll.viewtype.FloorViewType;
 import com.set.view.scroll.adapter.AllViewTypeAdapter;
 import com.set.view.scroll.adapter.OnItemClickListener;
 import com.set.view.scroll.entry.ItemListEntry;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +33,9 @@ public abstract class BaseGridActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.main_base_recyclerview);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
         mRecyclerView.setLayoutManager(gridLayoutManager);
+        //分割线
+        mRecyclerView.addItemDecoration(new RecycleViewDivider(2, getResources().getColor(R.color.color_d1)));
+
         routeItemEntries.addAll(initItemData());
 
         for (int m = 0; m < routeItemEntries.size(); m++) {
