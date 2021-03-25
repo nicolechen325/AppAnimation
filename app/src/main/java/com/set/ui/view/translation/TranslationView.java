f534ac68193655955a34747b4c81f92d4f5c91f0
chenguang <chenguang0508@163.com>
2021-03-25 22:37:55 +0800

    view移动

--- app/src/main/java/com/set/ui/view/translation/TranslationView.java
+++ app/src/main/java/com/set/ui/view/translation/TranslationView.java
@@ -9,20 +9,26 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 public class TranslationView extends View {
-    public static final String TAG_1 = "TranslationView";
+    public static final String TAG_1 = "TView";
     private Paint mPaint;
+    private boolean mEventRawXY = false;
+    public int type = 0;
     private int lastX;
     private int lastY;
-
     private int mScrollX;
     private int mScrollY;
+    private float mDownTranslationX;
+    private float mDownTranslationY;
 
-    private float mTranslationX;
-    private float mTranslationY;
+    private int sourceLeft;
+    private int sourceTop;
+    private int sourceRight;
+    private int sourceBottom;
 
     public TranslationView(Context context) {
         super(context);
@@ -45,68 +51,130 @@ public class TranslationView extends View {
         mPaint.setStyle(Paint.Style.FILL);
     }
 
+    public void useRawXY(boolean rawXY) {
+        mEventRawXY = rawXY;
+    }
+
+    public void setMoveView(int type) {
+        this.type = type;
+        useRawXY(type == 4);
+
+        lastX = 0;
+        lastY = 0;
+        mScrollX = 0;
+        mScrollY = 0;
+        mDownTranslationX = 0;
+        mDownTranslationY = 0;
+
+        //恢复位置
+        Log.d(TAG_1 + "Reset",
+                ":sourceLeft:" + sourceLeft +
+                        ",sourceTop:" + sourceTop +
+                        ",sourceRight:" + sourceRight +
+                        ",sourceBottom:" + sourceBottom);
+        layout(sourceLeft, sourceTop, sourceRight, sourceBottom);
+        setScrollX(0);
+        setScrollY(0);
+        setTranslationX(0);
+        setTranslationY(0);
+
+        invalidate();
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        final int action = event.getAction();
+
         final int x = (int) event.getX();
         final int y = (int) event.getY();
         final int rawx = (int) event.getRawX();
         final int rawy = (int) event.getRawY();
-        final int action = event.getAction();
+
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                Log.d(TAG_1 + "Touch", "ACTION_DOWN:x:" + x + ",y:" + y + ",rawx:" + rawx + ",rawy:" + rawy);
-                lastX = x;
-                lastY = y;
-//                lastX = rawx;
-//                lastY = rawy;
-                mTranslationX = getTranslationX();
-                mTranslationY = getTranslationY();
+                lastX = mEventRawXY ? rawx : x;
+                lastY = mEventRawXY ? rawy : y;
+                mDownTranslationX = getTranslationX();
+                mDownTranslationY = getTranslationY();
                 mScrollX = getScrollX();
                 mScrollY = getScrollY();
                 break;
             case MotionEvent.ACTION_MOVE:
-                Log.d(TAG_1 + "Touch", "ACTION_MOVE:x:" + x + ",y:" + y + ",rawx:" + rawx + ",rawy:" + rawy);
                 onMoveTouch(event);
                 break;
             case MotionEvent.ACTION_UP:
-                Log.d(TAG_1 + "Touch", "ACTION_UP:x:" + x + ",y:" + y + ",rawx:" + rawx + ",rawy:" + rawy);
                 break;
         }
+
+        logAction(action, x, y, rawx, rawy);
         return true;
     }
 
-
     private void onMoveTouch(MotionEvent event) {
-        final int currentX = (int) event.getX();//相对该视图坐标系
-        final int currentY = (int) event.getY();
+        final int x = (int) event.getX();//相对该视图坐标系
+        final int y = (int) event.getY();
+        final int rawx = (int) event.getRawX();//相对屏幕
+        final int rawy = (int) event.getRawY();
 
-//        final int currentX = (int) event.getRawX();//相对屏幕
-//        final int currentY = (int) event.getRawY();
+        final int currentX = mEventRawXY ? rawx : x;
+        final int currentY = mEventRawXY ? rawy : y;
+
         int offsetx = currentX - lastX;
         int offsety = currentY - lastY;
 
+        if (type == 0) {
+            layout(offsetx, offsety);
+        } else if (type == 1) {
+            offsetLeftAndRight(offsetx);  //对top和bottom偏移
+            offsetTopAndBottom(offsety);
+        } else if (type == 2) {
+            layoutParams(offsetx, offsety);
+        } else if (type == 3) {
+            scroll(offsetx, offsety);
+        } else if (type == 4) {
+            translationXY(offsetx, offsety);
+        }
+
+        Log.d(TAG_1 + "onMove",
+                ":offsetx:" + offsetx +
+                        ",offsety:" + offsety +
+                        ",mLeft:" + getLeft() +
+                        ",mTop:" + getTop() +
+                        ",mRight:" + getRight() +
+                        ",mBottom:" + getBottom() +
+                        ",translationX:" + getTranslationX() +
+                        ",translationY:" + getTranslationY() +
+                        ",mScrollX:" + getScrollX() +
+                        ",mScrollY:" + getScrollY());
+    }
+
+    void layout(int offsetx, int offsety) {
         //left、top、right、bottom基础上加上偏移量
         //改变 mLeft 和 mTop
         //需要 getX 和 getY
-        // 每次变化 offsetx ，累加到 mLeft 上，因为 mLeft变，currentX 是 相对视图坐标的，也跟着变，
+        //每次变化 offsetx ，累加到 mLeft 上，因为 mLeft变，currentX 是 相对视图坐标的，也跟着变，
         layout(getLeft() + offsetx,
                 getTop() + offsety,
                 getRight() + offsetx,
                 getBottom() + offsety);
+    }
 
+    void layoutParams(int offsetx, int offsety) {
+        LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) getLayoutParams();
+        layoutParams.leftMargin += offsetx;
+        layoutParams.topMargin += offsety;
+        setLayoutParams(layoutParams);
+    }
+
+    void scroll(int offsetx, int offsety) {
         //视图内部滚动
-//        scrollTo(mScrollX - offsetx, mScrollY - offsety);
-        Log.d(TAG_1 + "onMove", ":offsetx:" + offsetx + ",offsety:" + offsety +
-                ",mLeft:" + getLeft() +
-                ",mTop:" + getTop() +
-                ",mRight:" + getRight() +
-                ",mBottom:" + getBottom() +
-                ",translationX:" + getTranslationX() +
-                ",translationY:" + getTranslationY());
+        scrollTo(mScrollX - offsetx, mScrollY - offsety);
+    }
 
+    void translationXY(int offsetx, int offsety) {
         //需要 RawX 和 RawY
-//        setTranslationX(mTranslationX + offsetx);
-//        setTranslationY(mTranslationY + offsety);
+        setTranslationX(mDownTranslationX + offsetx);
+        setTranslationY(mDownTranslationY + offsety);
     }
 
     @Override
@@ -144,21 +212,43 @@ public class TranslationView extends View {
         int scrollX = getScrollX();
         int scrollY = getScrollY();
 
-        Log.d(TAG_1 + "Draw", "onDraw:" +
-                ",mLeft:" + mLeft +
-                ",mTop:" + mTop +
-                ",mRight:" + mRight +
-                ",mBottom:" + mBottom +
-                ",width:" + width +
-                ",height:" + height +
-                ",translationX:" + translationX +
-                ",translationY:" + translationY +
-                ",scrollX:" + scrollX +
-                ",scrollY:" + scrollY);
+        //记录原始状态位置
+        if (type == 0) {
+            sourceLeft = getLeft();
+            sourceTop = getTop();
+            sourceRight = getRight();
+            sourceBottom = getBottom();
+        }
+
+        Log.d(TAG_1 + "Draw",
+                "mLeft:" + mLeft +
+                        ",mTop:" + mTop +
+                        ",mRight:" + mRight +
+                        ",mBottom:" + mBottom +
+                        ",width:" + width +
+                        ",height:" + height +
+                        ",translationX:" + translationX +
+                        ",translationY:" + translationY +
+                        ",scrollX:" + scrollX +
+                        ",scrollY:" + scrollY);
 
         RectF rectF = new RectF(0, 0, width, height);
         canvas.drawRect(rectF, mPaint);
     }
 
-
+    void logAction(int action, int x, int y, int rawx, int rawy) {
+        String actStr = null;
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                actStr = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                actStr = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                actStr = "ACTION_UP";
+                break;
+        }
+        Log.d(TAG_1 + "Touch", actStr + ":x:" + x + ",y:" + y + ",rawx:" + rawx + ",rawy:" + rawy);
+    }
 }
--- app/src/main/java/com/set/ui/view/translation/ViewTranslationActivity.java
+++ app/src/main/java/com/set/ui/view/translation/ViewTranslationActivity.java
@@ -2,16 +2,21 @@ package com.set.ui.view.translation;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.set.ui.R;
 
 /**
- * View 属性
+ * View 位置属性
  */
-public class ViewTranslationActivity extends AppCompatActivity implements View.OnClickListener {
+public class ViewTranslationActivity extends AppCompatActivity {
     public static final String TAG_3 = "ViewTranslationActivity";
+
+    private TranslationView mTranslationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +26,38 @@ public class ViewTranslationActivity extends AppCompatActivity implements View.O
     }
 
     protected void initView() {
+        mTranslationView = findViewById(R.id.translation_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_view_transiation, menu);
+        return true;
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.img_tween_interpolator_1:
-                break;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        switch (id) {
+            case R.id.action_select0:
+                mTranslationView.setMoveView(0);
+                break;
+            case R.id.action_select1:
+                mTranslationView.setMoveView(1);
+                break;
+            case R.id.action_select2:
+                mTranslationView.setMoveView(2);
+                break;
+            case R.id.action_select3:
+                mTranslationView.setMoveView(3);
+                break;
+            case R.id.action_select4:
+                mTranslationView.setMoveView(4);
+                break;
         }
+
+        return true;
     }
 }
